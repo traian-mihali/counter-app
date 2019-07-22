040e5f9d8b22736cdc417af18d1ca8aeb0a922e4
Traian <traianmihali696@gmail.com>
2019-07-22 15:55:12 +0300

    add counter, counters and navbar components

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+  //   console.log("NavBar - Rendered");
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="https://google.com">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
